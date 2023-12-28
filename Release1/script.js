cff90fd0f0b786052ee0b0db1f5dc92a974c414e
George-user <rubix.channell@mail.ru>
2023-12-28 15:18:42 +0300

    add js for button

--- Release1/script.js
+++ Release1/script.js
@@ -22,3 +22,18 @@ function digitalClock(){
     todaysDay.classList.add('active')
 }
 setInterval(digitalClock, 1000)
+
+// Светлая/темная темы
+
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bx-moon");
+
+toggle.addEventListener("change", () => {
+  body.classList.toggle("dark");
+  sunIcon.className =
+    sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+  moonIcon.className =
+    moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+});
